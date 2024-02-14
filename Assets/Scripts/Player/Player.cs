add9ce4e713de520222675bbc0a5a2b4448a53e2
tyrann0s <work.tyran@gmail.com>
2024-02-14 12:18:55 +0600

    player sounds

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -18,6 +18,41 @@ public class Player : MonoBehaviour
     public Room CurrentRoom { get; private set; }
     public void SetRoom(Room room) { CurrentRoom = room; }
 
+    private bool isJumping;
+    private bool isRunning;
+
+    [Header("Sounds")]
+    [SerializeField]
+    [FMODUnity.EventRef]
+    private string walkEvent;
+    private FMOD.Studio.EventInstance walkSound;
+
+    [SerializeField]
+    [FMODUnity.EventRef]
+    private string runEvent;
+    private FMOD.Studio.EventInstance runSound;
+
+    [SerializeField]
+    [FMODUnity.EventRef]
+    private string jumpEvent;
+    private FMOD.Studio.EventInstance jumpSound;
+
+    [SerializeField]
+    [FMODUnity.EventRef]
+    private string landEvent;
+    private FMOD.Studio.EventInstance landSound;
+
+    [SerializeField]
+    private float walkHearingDeistance;
+    [SerializeField]
+    private float runHearingDeistance;
+    [SerializeField]
+    private float jumpAndLandHearingDeistance;
+
+    [SerializeField]
+    private float timeBetweenStep;
+    private float timeSinceLastFootstep;
+
     private void Start()
     {
         characterController = GetComponent<CharacterController>();
@@ -27,6 +62,11 @@ public class Player : MonoBehaviour
         Cursor.visible = false;
 
         CanMove = true;
+
+        walkSound = FMODUnity.RuntimeManager.CreateInstance(walkEvent);
+        runSound = FMODUnity.RuntimeManager.CreateInstance(runEvent);
+        jumpSound = FMODUnity.RuntimeManager.CreateInstance(jumpEvent);
+        landSound = FMODUnity.RuntimeManager.CreateInstance(landEvent);
     }
 
     public Vector3 GetEyesPosition()
@@ -40,7 +80,7 @@ public class Player : MonoBehaviour
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
         // Press Left Shift to run
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+        isRunning = Input.GetKey(KeyCode.LeftShift);
         float curSpeedX = CanMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Vertical") : 0;
         float curSpeedY = CanMove ? (isRunning ? runningSpeed : walkingSpeed) * Input.GetAxis("Horizontal") : 0;
         float movementDirectionY = moveDirection.y;
@@ -49,10 +89,19 @@ public class Player : MonoBehaviour
         if (Input.GetButton("Jump") && CanMove && characterController.isGrounded)
         {
             moveDirection.y = jumpForce;
+            jumpSound.start();
+            MakeNoise(jumpAndLandHearingDeistance);
         }
         else
         {
             moveDirection.y = movementDirectionY;
+        }
+
+        if (characterController.isGrounded && isJumping)
+        {
+            landSound.start();
+            MakeNoise(jumpAndLandHearingDeistance);
+            isJumping = false;
         }
 
         // Apply gravity. Gravity is multiplied by deltaTime twice (once here, and once below
@@ -61,6 +110,7 @@ public class Player : MonoBehaviour
         if (!characterController.isGrounded)
         {
             moveDirection.y -= gravity * Time.deltaTime;
+            isJumping = true;
         }
 
         // Move the controller
@@ -74,5 +124,40 @@ public class Player : MonoBehaviour
             playerCamera.transform.localRotation = Quaternion.Euler(rotationX, 0, 0);
             transform.rotation *= Quaternion.Euler(0, Input.GetAxis("Mouse X") * lookSpeed, 0);
         }
+
+        if (characterController.isGrounded)
+        {
+            if (curSpeedX != 0 || curSpeedY != 0) FootstepSound();
+        }
+    }
+
+    private void FootstepSound()
+    {
+        float tbsMod = 1;
+        if (isRunning)
+        {
+            tbsMod = 2;
+        }
+
+        if (Time.time - timeSinceLastFootstep >= timeBetweenStep / tbsMod)
+        {
+            if (isRunning)
+            {
+                runSound.start();
+                MakeNoise(runHearingDeistance);
+            }
+            else
+            {
+                walkSound.start();
+                MakeNoise(walkHearingDeistance);
+            }
+
+            timeSinceLastFootstep = Time.time; // Update the time since the last footstep sound
+        }
+    }
+
+    private void MakeNoise(float distance)
+    {
+
     }
 }
